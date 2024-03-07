492716e689122f13137004d679b16b2b75f1e6a3
Oyunbat <oyunbat9958@gmail.com>
2024-03-07 16:04:05 +0800

    window.document.queryselector oilgoson

--- app.js
+++ app.js
@@ -10,5 +10,12 @@ var roundScore = 0;
 //shoni ali talara busnig hadgalah huwisagch,  1-6 gesen utgig ene huwisagchid sanamsaguiger uusgej ugnu
 var dice = Math.floor(Math.random() * 6) + 1;
 //  <div class="player-score" id="score-0">43</div>
-window.document.querySelector("");
+// innerhtml dom - oor damjad html ashiglah bolomjig usgej bna
+// window.document.querySelector("#score-0").textContent = dice;
+// document.querySelector("#score-1").innerHTML = "<em> yes <em>";
+window.document.querySelector("#score-0").textContent = "0";
+window.document.querySelector("#score-1").textContent = "0";
+window.document.querySelector("#current-0").textContent = "0";
+window.document.querySelector("#current-1").textContent = "0";
+document.querySelector(".dice").style.display = "none";
 console.log("Шоо:" + dice);
