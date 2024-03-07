ede7fc893ca2c60075d11f952ac8dfbcfc7c836f
Oyunbat <oyunbat9958@gmail.com>
2024-03-07 15:10:09 +0800

    new project dice.

---
+++ app.js
@@ -0,0 +1,14 @@
+// toglogcin eljig hadgalah huwisagch , first player - 0 second player - 1
+var activePlayer = 1;
+
+//toglogcdin tsuglulsan onog tusglulah huwisagch
+var scores = [0, 0];
+
+//toglogcin eljinde tsuglulj baiga onog hadgalah huwisagch
+var roundScore = 0;
+
+//shoni ali talara busnig hadgalah huwisagch,  1-6 gesen utgig ene huwisagchid sanamsaguiger uusgej ugnu
+var dice = Math.floor(Math.random() * 6) + 1;
+//  <div class="player-score" id="score-0">43</div>
+window.document.querySelector("");
+console.log("Шоо:" + dice);
